a4ff3b093f83a58db750ee75223801889d8b9c5c
joemanf <joemanf0@gmail.com>
2022-10-12 14:16:02 -0400

    False alarm, it was working the whole time.

--- module/src/rotation.js
+++ module/src/rotation.js
@@ -117,9 +117,6 @@ async function rotateViaRotation(deltaX, deltaY, document, update){
     // Convert our delta to an angle, then adjust for the fact that the
     // rotational perspective in Foundry is shifted 90 degrees
     // counterclockwise.  
-    console.log('deltaX and deltaY', deltaX, deltaY)
-    console.log('core.pointToAngle(deltaX, deltaY) + 90', core.pointToAngle(deltaX, deltaY) + 90)
-    console.log('core.pointToAngle(deltaX, deltaY) - 90', core.pointToAngle(deltaX, deltaY) - 90)
     update.rotation = core.normalizeDegrees(
         core.pointToAngle(deltaX, deltaY) - 90
     );
@@ -160,10 +157,6 @@ async function rotateTokenOnPreUpdate(token_document, change, options, userId) {
     if (newX === token_data.x && newY === token_data.y) {
         return;
     }
-
-    console.log('token_data.x', token_data.x)
-    console.log('token_data.y', token_data.y)
-    console.log('change', change)
 
     const deltaX = newX - token_data.x;
     const deltaY = newY - token_data.y;
